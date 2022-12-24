f9304bf2df34061bd65bf9d0c06b6610d70f8601
jervincarlos <jervin@sliqpay.com>
2022-12-24 13:28:27 +0800

    wip

--- app.js
+++ app.js
@@ -8,19 +8,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
-const userRoute = require("./routes/userRoute");
+const userRoute = require("./src/routes/userRoute");
 app.use("/api/users", userRoute);
 
-const expenseRoute = require("./routes/expenseRoute");
+const expenseRoute = require("./src/routes/expenseRoute");
 app.use("/api/expense", expenseRoute);
 
-const balanceRoute = require("./routes/balanceRoute");
+const balanceRoute = require("./src/routes/balanceRoute");
 app.use("/api/balance", balanceRoute);
 
-const paymentRoute = require("./routes/paymentRoute");
+const paymentRoute = require("./src/routes/paymentRoute");
 app.use("/api/payment", paymentRoute);
 
-const authRoute = require("./routes/authRoute");
+const authRoute = require("./src/routes/authRoute");
 app.use("/api/auth", authRoute);
 
 const port = process.env.PORT || 3001;
